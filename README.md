c2a18879f2a4728f3f60491ae0acbd7ebb2c3864
Christian Broberg <cb@webhouse.dk>
2017-10-24 16:40:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# odeum-code-lite
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+	render() {
+		return (
+            <div>
+                <p>To get started, edit <code>src/App.js</code> and save to reload.</p>
+            </div>
+		)
+	}
+}
+
+export default App
