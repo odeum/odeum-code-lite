5c9b917b30f90ab86a50401ea7902de1e6b781e9
Andrei Tudor <andrei.tudor27@gmail.com>
2017-11-04 14:19:27 +0100

    Added Source Sans Pro

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom'
 // import App from './App'
 import ReactRouter from 'ReactRouter'
 import registerServiceWorker from './registerServiceWorker'
+import './index.css'
+require('typeface-source-sans-pro')
 
 render(<ReactRouter />, document.getElementById('root'))
 registerServiceWorker()
