aaf8d0bdbee111eaf994d1624a62288c2f87744e
Christian Broberg <christianbroberg@CB-MacBook-Pro.lan>
2017-12-19 00:18:49 +0100

    Public API for Protected content (Login)

--- docs/App_minimal.js
+++ docs/App_minimal.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { AppContainer, Header, MenuPanel, Menu, Footer } from 'odeum-app'
+import { AppContainer, Header, MenuPanel, Menu, Footer, Login, Protected } from 'odeum-app'
 import { TabPanel, Tab } from 'odeum-ui'
 
 class App extends Component {
@@ -7,23 +7,36 @@ class App extends Component {
 		super(props)
 
 		this.state = {
-			menuIsOpen: true,
-			helpId: ''
+			open: true,
+			helpId: '',
+			isLoggedIn: false
 		}			
 	}
 	
 	render() {
+		const { open, helpId, isLoggedIn } = this.state
 		return (
 			<AppContainer>
 				<Header />
-				<MenuPanel>
+				<MenuPanel open={open}>
 					<Menu>
-						<TabPanel>							
-							<Tab />
-							<Tab />
-							<Tab />
-						</TabPanel>
+						<Tab>
+							<Login />
+						</Tab>
+						<Tab>
+							Tab content ...
+						</Tab>
 					</Menu>
+					<Protected isLoggedIn={isLoggedIn}>
+						<Menu>
+							<Tab>
+								Tab content ...
+							</Tab>
+							<Tab>
+								Tab content ...
+							</Tab>
+						</Menu>
+					</Protected>
 				</MenuPanel>
 				<Footer/>
 			</AppContainer>
@@ -33,11 +46,4 @@ class App extends Component {
 
 export default App
 
-/* Outputs:
-
-Menu Item
-	Tab
-	Tab
-	Tab
-*/
 
