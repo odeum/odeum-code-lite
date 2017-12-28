f0f9be78b3056d44a474b58bca287071c35e7552
Andrei <andrei.tudor27@gmail.com>
2017-12-28 14:45:35 +0100

    IE11 includes function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'core-js/es6/map'
 import 'core-js/es6/set'
+import 'core-js/fn/string/includes'
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 // import { render } from 'react-dom'
